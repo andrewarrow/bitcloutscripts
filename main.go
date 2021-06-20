c65f02910af8f51916d4a974d0755b0c1d31728a
Andrew Arrow <oneone@gmail.com>
2021-06-20 05:50:55 -0700

    adding cli menu

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bcs/args"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func PrintHelp() {
+	fmt.Println("")
+	fmt.Println("  bcs posts       # print all posts")
+	fmt.Println("  bcs graph       # make clout.gv graph file")
+	fmt.Println("")
+}
+
+var argMap map[string]string
 
 func main() {
-	fmt.Println("bitcloutscripts")
+	rand.Seed(time.Now().UnixNano())
+
+	if len(os.Args) == 1 {
+		PrintHelp()
+		return
+	}
+	command := os.Args[1]
+	argMap = args.ToMap()
+
+	if command == "posts" {
+		HandlePosts()
+	} else if command == "graph" {
+		HandleGraph()
+	} else {
+		fmt.Println("not a command yet")
+	}
+
 }
